dae2f7c60ceba5dd0e0bfd98abd19b69dd9555d3
Douglas <douglaspereira04@hotmail.com>
2022-04-29 17:09:56 -0300

    changes navigation menu layout

--- view/side_menu.py
+++ view/side_menu.py
@@ -9,28 +9,41 @@ class SideMenu(QtWidgets.QWidget):
         self.setLayout(self.layout)
         self.title = QtWidgets.QLabel('Menu de funções')
         self.list = QtWidgets.QListWidget()
-        self.list.setMaximumWidth(100)
         self.list.setMaximumHeight(100)
         self.add_btn = QtWidgets.QPushButton('Add')
         self.step = QLineEdit()
         self.step.setPlaceholderText("Navigation Steps");
         self.step.setValidator(QIntValidator());
-        self.left_btn = QtWidgets.QPushButton('Left')
-        self.right_btn = QtWidgets.QPushButton('Right')
-        self.up_btn = QtWidgets.QPushButton('Up')
-        self.down_btn = QtWidgets.QPushButton('Down')
-        self.zin_btn = QtWidgets.QPushButton('Zoom In')
-        self.zout_btn = QtWidgets.QPushButton('Zoom Out')
+
+        self.steps_label = QtWidgets.QLabel('Steps')
+        self.zoom_label = QtWidgets.QLabel('Zoom')
+
+        self.left_btn = QtWidgets.QPushButton('<')
+        self.right_btn = QtWidgets.QPushButton('>')
+        self.up_btn = QtWidgets.QPushButton('^')
+        self.down_btn = QtWidgets.QPushButton('v')
+        self.zin_btn = QtWidgets.QPushButton('+')
+        self.zout_btn = QtWidgets.QPushButton('-')
+
+        self.navigation_layout = QtWidgets.QGridLayout()
+
+
         self.layout.addWidget(self.title)
         self.layout.addWidget(self.list)
         self.layout.addWidget(self.add_btn)
-        self.layout.addWidget(self.step)
-        self.layout.addWidget(self.left_btn)
-        self.layout.addWidget(self.right_btn)
-        self.layout.addWidget(self.up_btn)
-        self.layout.addWidget(self.down_btn)
-        self.layout.addWidget(self.zin_btn)
-        self.layout.addWidget(self.zout_btn)
+        self.layout.addLayout(self.navigation_layout)
+
+        self.navigation_layout.addWidget(self.steps_label, 0,0)
+        self.navigation_layout.addWidget(self.step, 0,1,1,2)
+        self.navigation_layout.addWidget(self.up_btn, 1,1)
+        self.navigation_layout.addWidget(self.left_btn, 2,0)
+        self.navigation_layout.addWidget(self.right_btn,2,2)
+        self.navigation_layout.addWidget(self.down_btn, 3,1)
+        self.navigation_layout.addWidget(self.zoom_label, 4,0)
+        self.navigation_layout.addWidget(self.zin_btn, 4,1)
+        self.navigation_layout.addWidget(self.zout_btn, 4,2)
+
+        self.setMaximumWidth(150)
 
 
     def make_list(self,obj_list):

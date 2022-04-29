6ffb8d846d3fd44ffea9a98fa2e1405f69bf0232
Douglas <douglaspereira04@hotmail.com>
2022-04-29 16:44:37 -0300

    adds log text field

--- view/graphics_window.py
+++ view/graphics_window.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPlainTextEdit
 from view.canvas import Canvas
 from view.side_menu import SideMenu
 
@@ -7,9 +8,14 @@ class GraphicsWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         ## Generate the structure parts of the MainWindow
         self.central_widget = QtWidgets.QWidget()  # A QWidget to work as Central Widget
+        self.right_widget = QtWidgets.QWidget()  # A QWidget to work as Central Widget
         self.layout1 = QtWidgets.QVBoxLayout()  # Vertical Layout
         self.layout2 = QtWidgets.QHBoxLayout()  # Horizontal Layout
+        self.layout3 = QtWidgets.QVBoxLayout()  # Vorizontal Layout
         self.canvas = Canvas()
+        self.log = QPlainTextEdit(self)
+        self.log.setReadOnly(True)
+
         self.side_menu = SideMenu()
         # self.exitBtn = QtWidgets.QPushButton('Exit')
         ## Build the structure
@@ -17,4 +23,8 @@ class GraphicsWindow(QtWidgets.QMainWindow):
         self.central_widget.setLayout(self.layout1)
         self.layout1.addLayout(self.layout2)
         self.layout2.addWidget(self.side_menu)
-        self.layout2.addWidget(self.canvas)
+        self.layout2.addLayout(self.layout3)
+        self.layout3.addWidget(self.canvas)
+        self.layout3.addWidget(self.log)
+
+        self.log.setFixedHeight(60)

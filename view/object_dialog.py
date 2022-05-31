5fc52dfc77ca9c84207617c78f7fcdeeeb1b87f7
Douglas <douglaspereira04@hotmail.com>
2022-05-31 20:52:27 -0300

    removes bezier from view

--- view/object_dialog.py
+++ view/object_dialog.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QDialog, QLineEdit, QPlainTextEdit, QDialogButtonBox
 import numbers
 
 class ObjectDialog(QDialog):
-    def __init__(self, parent=None, name="", coords="", color="", filled=False, bezier=False):
+    def __init__(self, parent=None, name="", coords="", color="", filled=False):
         super().__init__(parent)
 
         self.setWindowTitle("Object")
@@ -15,10 +15,6 @@ class ObjectDialog(QDialog):
         self.filled.setEnabled(False)
         self.coordinates = QPlainTextEdit(coords)
         self.buttonBox = None
-
-        self.bezier = QCheckBox()
-        self.bezier.setChecked(bezier)
-        self.bezier.setEnabled(False)
 
 
         self.name.setPlaceholderText("Name")
@@ -32,44 +28,24 @@ class ObjectDialog(QDialog):
         layout = QFormLayout(self)
         layout.addRow("Name: ", self.name)
         layout.addRow("Color: ", self.color)
+        layout.addRow("Filled: ", self.filled)
         layout.addRow("Coordinates: ", self.coordinates)
         layout.addWidget(buttonBox)
-        layout.addRow("Filled: ", self.filled)
-        layout.addRow("Bezier Curve: ", self.bezier)
 
 
         self.coordinates.textChanged.connect(self.set_filled_state)
-        self.bezier.stateChanged.connect(self.set_filled_state)
-        self.filled.stateChanged.connect(self.set_filled_state)
         self.set_filled_state()
 
     def get_inputs(self):
-        return (self.name.text(), self.coordinates.toPlainText(), self.color.text(), self.filled.isChecked(), self.bezier.isChecked())
+        return (self.name.text(), self.coordinates.toPlainText(), self.color.text(), self.filled.isChecked())
 
     def set_filled_state(self):
-
         try:
             obj_list = list(eval(self.coordinates.toPlainText()))
             if(len(obj_list)>2 and all(isinstance(x, numbers.Number) and isinstance(y, numbers.Number) for (x,y) in obj_list)):
                 self.filled.setEnabled(True)
-                self.bezier.setEnabled(True)
             else:
-                self.filled.setChecked(False)
                 self.filled.setEnabled(False)
-                self.bezier.setChecked(False)
-                self.bezier.setEnabled(False)
         except Exception as e:
-            self.filled.setChecked(False)
             self.filled.setEnabled(False)
-            self.bezier.setChecked(False)
-            self.bezier.setEnabled(False)
-
-        if(self.bezier.isChecked()):
-            self.filled.setChecked(False)
-            self.filled.setEnabled(False)
-
-        if(self.filled.isChecked()):
-            self.bezier.setChecked(False)
-            self.bezier.setEnabled(False)
-
 
